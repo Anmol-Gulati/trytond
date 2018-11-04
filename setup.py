a0691a646e14897f1fe4788a8265b5f6d3bf236c
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2018-11-04 23:34:45 +0530

    bumped dev version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ else:
     pg_require = ['psycopg2 >= 2.5']
 
 setup(name=name,
-    version=version,
+    version=version + 'pre1',
     description='Tryton server',
     long_description=read('README'),
     author='Tryton',
