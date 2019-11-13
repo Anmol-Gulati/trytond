e28aa14d7f5fc11ff036531173aedcdef9129810
Prakash Pandey <prakash.pandey@fulfil.io>
2019-11-13 19:25:39 +0530

    Bump version: 4.0.31.0.dev2 → 4.0.31.0.dev3

--- trytond/__init__.py
+++ trytond/__init__.py
@@ -5,7 +5,7 @@ import time
 import logging
 from email import charset
 
-__version__ = "4.0.31.0.dev2"
+__version__ = "4.0.31.0.dev3"
 logger = logging.getLogger(__name__)
 
 os.environ['TZ'] = 'UTC'
