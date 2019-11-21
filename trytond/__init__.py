690e9682ccaefb68a4cad45505e73754c66c8be3
Tarun Bhardwaj <tarun.bhardwaj@fulfil.io>
2019-11-21 16:11:49 +0530

    Released version 4.0.31.0.dev4

--- trytond/__init__.py
+++ trytond/__init__.py
@@ -5,7 +5,7 @@ import time
 import logging
 from email import charset
 
-__version__ = "4.0.31.0.dev3"
+__version__ = "4.0.31.0.dev4"
 logger = logging.getLogger(__name__)
 
 os.environ['TZ'] = 'UTC'
